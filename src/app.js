15f1c883cb657bef6efb9272adda69d46f6915db
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-03 22:32:39 +0530

    Test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('App.js is running')
 
 //JSX - JavaScript XML
-var template = <p>Does this change?</p>;
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
