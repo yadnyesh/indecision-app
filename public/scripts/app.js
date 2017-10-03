15b9a110038b67069bac13835b550132e0d32dc8
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-03 21:57:54 +0530

    First React code with help from Babel.io

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 console.log('App.js is running')
+
+//JSX - JavaScript XML
+//var template = <p>This is JSX from App.js</p>;
+var template = React.createElement(
+  "h1",
+  null,
+  "This is JSX from App.js"
+);
+var appRoot = document.getElementById('app');
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
