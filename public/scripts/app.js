c98a763b785dbd06da5405d9cded3b93a8dcae19
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-04 08:34:17 +0530

    With JSX Parent tag

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,21 @@
 'use strict';
 
+console.log('App.js is running');
+
 //JSX - JavaScript XML
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'Does this change?'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info.'
+  )
 );
 var appRoot = document.getElementById('app');
 
