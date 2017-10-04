ea4a07e84d2bad80b20b9b4ce4643973d639e059
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-04 08:36:03 +0530

    With JSX parenthesis format

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 console.log('App.js is running')
 
 //JSX - JavaScript XML
-var template = <div><h1>Indecision App</h1><p>This is some info.</p></div>;
+var template = (
+<div>
+  <h1>Indecision App</h1>
+  <p>This is some info.</p>
+</div>
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
