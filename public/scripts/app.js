875ef01962d7d8cbe444b05b5899bbcd145a4d17
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-10-29 18:29:25 +0530

    Few Changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,53 +10,101 @@ var app = {
   options: ['One', 'Two']
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options ' : 'No Options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item One'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item Two'
-    )
-  )
-);
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  console.log('Form Submitted');
+  var option = e.target.elements.option.value;
 
-function getLocation(location) {
-  if (location) return React.createElement(
-    'p',
-    null,
-    'Location: ',
-    location
-  );
-}
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+  console.log(app.options);
+  render();
+};
 
-var user = {
-  name: 'Yadnyesh',
-  age: 36,
-  location: 'Goa'
+var clearOptions = function clearOptions() {
+  app.options = [];
+  render();
 };
 
 var appRoot = document.getElementById('app');
+
+var numbers = [55, 101, 1000];
+
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options ' : 'No Options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    [React.createElement(
+      'p',
+      { key: '1' },
+      'a'
+    ), React.createElement(
+      'p',
+      { key: '2' },
+      'b'
+    ), React.createElement(
+      'p',
+      { key: '3' },
+      'c'
+    ), React.createElement(
+      'p',
+      { key: '4' },
+      'd'
+    )],
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item One'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item Two'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      ),
+      React.createElement(
+        'button',
+        { onClick: clearOptions },
+        'Remove All'
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -8,28 +8,52 @@ const app = {
   options: ['One', 'Two']
 };
 
-const template = (
-<div>
-  <h1>{app.title}</h1>
-  {app.subtitle && <p>{app.subtitle}</p>}
-  <p>{app.options.length> 0 ? 'Here are your options ' : 'No Options'}</p>
-  <ol>
-    <li>Item One</li>
-    <li>Item Two</li>
-  </ol>
-</div>
-);
 
-function getLocation(location) {
-  if(location)
-    return <p>Location: {location}</p>;
-}
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  console.log('Form Submitted');
+  const option = e.target.elements.option.value;
 
-const user = {
-  name: 'Yadnyesh',
-  age: 36,
-  location: 'Goa'
+  if(option){
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+  console.log(app.options);
+  render();
+};
+
+const clearOptions = () => {
+  app.options = [];
+  render();
 };
 
 const appRoot = document.getElementById('app');
 
+const numbers = [55, 101, 1000];
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length> 0 ? 'Here are your options ' : 'No Options'}</p>
+      <p>{app.options.length}</p>
+      {
+        [<p key="1">a</p>,<p key="2">b</p>,<p key="3">c</p>,<p key="4">d</p>]
+      }
+      <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+        <button onClick={clearOptions}>Remove All</button>
+      </form>
+    </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+};
+
+render();
