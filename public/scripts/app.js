2bb4ea2f6d4b7ed06ced85bab4cda43d3d2304a3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-05 14:25:17 +0530

    Handle Add, Minus and Reset One functions

---
+++ public/scripts/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Counter = function (_React$Component) {
+  _inherits(Counter, _React$Component);
+
+  function Counter() {
+    _classCallCheck(this, Counter);
+
+    return _possibleConstructorReturn(this, (Counter.__proto__ || Object.getPrototypeOf(Counter)).apply(this, arguments));
+  }
+
+  _createClass(Counter, [{
+    key: 'handleAddOne',
+    value: function handleAddOne() {
+      console.log('Handle Add One Clicked...');
+    }
+  }, {
+    key: 'handleMinusOne',
+    value: function handleMinusOne() {
+      console.log('Handle Minus one ...button clicked');
+    }
+  }, {
+    key: 'handleReset',
+    value: function handleReset() {
+      console.log('Handle Reset ..clicked');
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Count: '
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleAddOne },
+          '+1'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleMinusOne },
+          '-1'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleReset },
+          'Reset'
+        )
+      );
+    }
+  }]);
+
+  return Counter;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Counter, null), document.getElementById('app'));
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,26 +1,29 @@
 class Counter extends React.Component {
+
+  handleAddOne() {
+    console.log('Handle Add One Clicked...');
+  }
+
+  handleMinusOne(){
+    console.log('Handle Minus one ...button clicked');
+  }
+
+  handleReset(){
+    console.log('Handle Reset ..clicked');
+  }
+
   render() {
     return(
       <div>
         <h1>Count: </h1>
-        <button>+1</button>
-        <button>-1</button>
-        <button>Reset</button>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     );
   }
 }
 
-handleAddOne() {
-  console.log('Handle Add One Clicked...');
-}
 
-handleMinusOne(){
-  console.log('Handle Minus one ...button clicked');
-}
-
-handleReset(){
-  console.log('Handle Reset ..clicked');
-}
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
