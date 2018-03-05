77eef8e6f9e4083818922827beb00add83067006
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-05 19:00:04 +0530

    Remove Unused code

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -29,32 +29,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
-
-
-
-//*****************Old Code*******************
-// const appRoot = document.getElementById('app');
-
-// let showText = true;
-
-// const toggleText = () => {
-//   showText = !showText;
-//   //console.log(showText);
-//   render();
-// }
-
-// const render = () => {
-
-//   const template = (
-//     <div>
-//       <h1>Visibility Toggle</h1>
-//       <button onClick={toggleText}>{showText ? 'Hide Details' : 'Show Details'}</button>
-//       <p> {showText ? 'Hey! You can see some details now' : ''} </p>
-//       {console.log(showText)}
-//     </div>
-//   );
-
-//   ReactDOM.render(template, appRoot);
-// }
-
-// render();
