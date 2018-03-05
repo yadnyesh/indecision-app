1e44aaa43563660bb380540775fff4140b036647
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-05 15:33:22 +0530

    Commenting out the old code

--- src/playground/build-it-visible.js
+++ src/playground/build-it-visible.js
@@ -1,25 +1,31 @@
-const appRoot = document.getElementById('app');
 
-let showText = true;
 
-const toggleText = () => {
-  showText = !showText;
-  //console.log(showText);
-  render();
-}
 
-const render = () => {
 
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggleText}>{showText ? 'Hide Details' : 'Show Details'}</button>
-      <p> {showText ? 'Hey! You can see some details now' : ''} </p>
-      {console.log(showText)}
-    </div>
-  );
 
-  ReactDOM.render(template, appRoot);
-}
+//*****************Old Code*******************
+// const appRoot = document.getElementById('app');
 
-render();
+// let showText = true;
+
+// const toggleText = () => {
+//   showText = !showText;
+//   //console.log(showText);
+//   render();
+// }
+
+// const render = () => {
+
+//   const template = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <button onClick={toggleText}>{showText ? 'Hide Details' : 'Show Details'}</button>
+//       <p> {showText ? 'Hey! You can see some details now' : ''} </p>
+//       {console.log(showText)}
+//     </div>
+//   );
+
+//   ReactDOM.render(template, appRoot);
+// }
+
+// render();
