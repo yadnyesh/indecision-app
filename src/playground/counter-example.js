4f5c59c0a517ed779044a6562aa78fe304141a85
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2018-03-05 14:44:38 +0530

    Added constructor

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,4 +1,10 @@
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne   = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset    = this.handleReset.bind(this);
+  }
 
   handleAddOne() {
     console.log('Handle Add One Clicked...');
